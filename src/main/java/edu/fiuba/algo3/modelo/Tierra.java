6b6b3ff8cf053c6bd6b6d11e3e725f27c9e5acbb
Lisandro <lisandro.romanturrisi@gmail.com>
2023-05-31 17:05:17 -0300

    Es posible construir si esta vacia la tierra. No sera posible construir si la misma no esta vacia

--- src/main/java/edu/fiuba/algo3/modelo/Tierra.java
+++ src/main/java/edu/fiuba/algo3/modelo/Tierra.java
@@ -1,12 +1,18 @@
 package edu.fiuba.algo3.modelo;
 
+
 public class Tierra {
 	
 	private Defensa defensa;
 	
 	
+	
+	
 	private Boolean puedoConstruir(){
 		//checkear que el atributo este lleno ¿Como hago eso?
+		if(this.defensa!=null) {
+			return false;
+		}
 		return true;
 	}
 	
