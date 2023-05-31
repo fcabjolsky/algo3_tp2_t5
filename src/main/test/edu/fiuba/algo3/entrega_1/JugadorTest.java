0fbb2035809580bd00d7c44475611b70b6bf8cf7
Caluav <camilaavalos7701@gmail.com>
2023-05-31 17:40:21 -0300

    git commit

--- src/main/test/edu/fiuba/algo3/entrega_1/JugadorTest.java
+++ src/main/test/edu/fiuba/algo3/entrega_1/JugadorTest.java
@@ -3,12 +3,11 @@ package edu.fiuba.algo3.entrega_1;
 import edu.fiuba.algo3.Jugador;
 import edu.fiuba.algo3.Defensa;
 import edu.fiuba.algo3.NoDisponeDeSuficientesCreditos;
-import edu.fiuba.algo3.Defensa;
-import org.junit.Test;
-import static org.junit.Assert.*;
-import org.junit.Rule;
-import org.junit.rules.ExpectedException;
-import static org.mockito.Mockito.*;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class JugadorTest {
 
@@ -23,14 +22,12 @@ public class JugadorTest {
         assertEquals(creditosEsperados, jugador.getCreditos());
     }
 
-    @Rule
-    public ExpectedException thrown = ExpectedException.none();
     @Test
-    public void elJugadorSoloConstruyeSiTieneSuficientesCreditos() throws Exception {
+    public void elJugadorSoloConstruyeSiTieneSuficientesCreditos(){
         Jugador jugador = new Jugador();
-        Defensa torreBlanca = mock(Defensa.class);
-        when(torreBlanca.puedeConstruir(100)).thenReturn(false);
-        thrown.expect(NoDisponeDeSuficientesCreditos.class);
-        jugador.construir(torreBlanca);
+        Defensa torreBlanca = Mockito.mock(Defensa.class);
+        Mockito.when(torreBlanca.puedeConstruir(100)).thenReturn(false);
+        assertThrows(NoDisponeDeSuficientesCreditos.class, () -> jugador.construir(torreBlanca));
+
     }
 }
