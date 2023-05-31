1d9c4a7430a9fe85471ce73ff6d305f96f702b73
Camila <107652029+Caluav@users.noreply.github.com>
2023-05-31 16:56:29 -0300

    Update NoDisponeDeSuficientesCreditos.java

--- src/main/java/edu/fiuba/algo3/NoDisponeDeSuficientesCreditos.java
+++ src/main/java/edu/fiuba/algo3/NoDisponeDeSuficientesCreditos.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package Model;
 
-/**
- *
- * @author kamic
- */
+package edu.fiuba.algo3;
+
 public class NoDisponeDeSuficientesCreditos extends RuntimeException{
     
 }
