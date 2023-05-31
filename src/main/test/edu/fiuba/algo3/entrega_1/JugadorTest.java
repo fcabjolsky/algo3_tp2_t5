a1f278f72bf769de10878edfd5ea03f6129f72ee
Camila <107652029+Caluav@users.noreply.github.com>
2023-05-31 16:20:51 -0300

    Add files via upload

---
+++ src/main/test/edu/fiuba/algo3/entrega_1/JugadorTest.java
@@ -0,0 +1,33 @@
+
+package Model;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
+import static org.mockito.Mockito.*;
+
+public class JugadorTest {
+
+    public JugadorTest() {
+    }
+
+    @Test
+    public void elJugadorEmpiezaConLaVidaYcreditosCorrectos() {
+        Jugador jugador = new Jugador();
+        int vidaEsperada = 20, creditosEsperados = 100;
+        assertEquals(vidaEsperada, jugador.getVida());
+        assertEquals(creditosEsperados, jugador.getCreditos());
+    }
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+    @Test
+    public void elJugadorSoloConstruyeSiTieneSuficientesCreditos() throws Exception {
+        Jugador jugador = new Jugador();
+        Defensa torreBlanca = mock(Defensa.class);
+        when(torreBlanca.costoDeConstruccion()).thenReturn(200);
+        thrown.expect(NoDisponeDeSuficientesCreditos.class);
+        jugador.construir(torreBlanca);
+    }
+}
