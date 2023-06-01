98c1bd5c503df7e1e12ad11ed13518d9137a7868
Lisandro <lisandro.romanturrisi@gmail.com>
2023-06-01 17:51:25 -0300

    agregando los archivos correspondientes

---
+++ src/main/java/edu/fiuba/algo3/Rango.java
@@ -0,0 +1,17 @@
+package edu.fiuba.algo3;
+
+public class Rango {
+    private int radio;
+
+    private int getRadio() {
+        return this.radio;
+    }
+
+    public Rango(int radio) {
+        this.radio = radio;
+    }
+
+    public boolean estaEnRango(Posicion posicion) {
+        return true;
+    }
+}
