20b43de3cfad677d5364219cf822339dbe389b1b
Lisandro <lisandro.romanturrisi@gmail.com>
2023-06-01 11:30:59 -0300

    Se verifica lo contrario, no es posible construir en rocoso ni pasarelas

---
+++ src/main/test/edu/fiuba/algo3/entrega_1/RocosoTest.java
@@ -0,0 +1,25 @@
+package edu.fiuba.algo3.entrega_1;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import org.junit.jupiter.api.Test;
+
+import edu.fiuba.algo3.modelo.Rocoso;
+import edu.fiuba.algo3.modelo.Torre;
+
+public class RocosoTest {
+	
+	@Test
+    public void noEsPosibleConstruirDefesasSobreTerrenoRocoso() {
+    	//Arrange
+    	
+    	Rocoso nuevoRocoso = new Rocoso();
+    	
+    	Torre torrePrimera = new Torre();
+    	
+    	
+    	//Act - Assert
+    	
+    	assertFalse(nuevoRocoso.agregarDefensa(torrePrimera));
+    }
+}
