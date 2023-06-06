30c22579b0f4c900f2bb2908a01827566cb998ce
Caluav <camilaavalos7701@gmail.com>
2023-06-06 12:37:22 -0300

    Se le agragaron test a jugador de muertes de Enemigos

--- src/main/test/edu/fiuba/algo3/entrega_1/JugadorTest.java
+++ src/main/test/edu/fiuba/algo3/entrega_1/JugadorTest.java
@@ -3,6 +3,9 @@ package edu.fiuba.algo3.entrega_1;
 import edu.fiuba.algo3.modelo.Jugador;
 import edu.fiuba.algo3.modelo.Defensa;
 import edu.fiuba.algo3.modelo.Posicion;
+import edu.fiuba.algo3.modelo.Hormiga;
+import edu.fiuba.algo3.modelo.Arania;
+import edu.fiuba.algo3.modelo.Contador;
 import edu.fiuba.algo3.modelo.NoDisponeDeSuficientesCreditos;
 
 import org.junit.jupiter.api.Test;
@@ -31,6 +34,55 @@ public class JugadorTest {
         Mockito.when(torreBlanca.puedeConstruir(100)).thenReturn(false);
         assertThrows(NoDisponeDeSuficientesCreditos.class, () -> jugador.construir(torreBlanca, posicion));
 
+    }
+    @Test
+    public void AlDestruir3EnemigosHormigasSeLeAsignanAlJugadorLosCreditosCorrespondientes(){
+        Jugador jugador = new Jugador();
+        Posicion posicion= new Posicion(1, 2);
+        Hormiga hormiga = new Hormiga(posicion);
+        Jugador jugadorEsperado = new Jugador(20, 103);
+        Contador muertesDeHormigas = new Contador();
+
+        hormiga.morir(jugador, muertesDeHormigas);
+        hormiga.morir(jugador, muertesDeHormigas);
+        hormiga.morir(jugador, muertesDeHormigas);
+
+        assertEquals(jugadorEsperado, jugador);
+    }
+    @Test
+    public void AlDestruir11EnemigosHormigasSeLeAsignanAlJugadorLosCreditosCorrespondientes(){
+        Jugador jugador = new Jugador();
+        Posicion posicion= new Posicion(1, 2);
+        Hormiga hormiga = new Hormiga(posicion);
+        Jugador jugadorEsperado = new Jugador(20, 112);
+        Contador muertesDeHormigas = new Contador();
+
+        hormiga.morir(jugador, muertesDeHormigas);
+        hormiga.morir(jugador, muertesDeHormigas);
+        hormiga.morir(jugador, muertesDeHormigas);
+        hormiga.morir(jugador, muertesDeHormigas);
+        hormiga.morir(jugador, muertesDeHormigas);
+        hormiga.morir(jugador, muertesDeHormigas);
+        hormiga.morir(jugador, muertesDeHormigas);
+        hormiga.morir(jugador, muertesDeHormigas);
+        hormiga.morir(jugador, muertesDeHormigas);
+        hormiga.morir(jugador, muertesDeHormigas);
+        hormiga.morir(jugador, muertesDeHormigas);
+
+        assertEquals(jugadorEsperado, jugador);
+    }
+
+    @Test
+    public void AlDestruirUnEnemigoAraniaSeLeAsignanAlJugadorCreditos(){
+        Jugador jugador = new Jugador();
+        Posicion posicion= new Posicion(1, 2);
+        Arania arania = new Arania(posicion);
+        Jugador jugadorOriginal = new Jugador(20, 100);
+        Contador muertesDeAranias = new Contador();
+
+        arania.morir(jugador, muertesDeAranias);
+
+        assertNotEquals(jugadorOriginal, jugador);
     }
 
 }
