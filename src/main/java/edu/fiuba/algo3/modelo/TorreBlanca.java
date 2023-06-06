a98eb8823280727ff3c74d027752894b56e02fcc
Caluav <camilaavalos7701@gmail.com>
2023-06-06 13:34:53 -0300

    refactor test 03- Se implemento el metodo construir

--- src/main/java/edu/fiuba/algo3/modelo/TorreBlanca.java
+++ src/main/java/edu/fiuba/algo3/modelo/TorreBlanca.java
@@ -4,7 +4,21 @@ public class TorreBlanca extends Torre {
 
 
     public TorreBlanca(Posicion posicion) {
-        super(1);
+        super(1, 10);
         this.rango = new Rango(3, posicion);
     }
+
+    public TorreBlanca(int tiempoDeConstruccion, int costo) {
+        super(tiempoDeConstruccion, costo);
+    }
+
+    public Defensa construir(Jugador jugador, Posicion posicion) {
+        TorreBlanca torreADevolver = new TorreBlanca(posicion);
+        if (jugador.puedeConstruir(super.costo)){
+            jugador.restarCreditos(super.costo);
+             torreADevolver = new TorreBlanca(posicion);
+        }
+        return torreADevolver;
+    }
+
 }
--- src/main/java/edu/fiuba/algo3/modelo/TorrePlateada.java
+++ src/main/java/edu/fiuba/algo3/modelo/TorrePlateada.java
@@ -3,7 +3,20 @@ package edu.fiuba.algo3.modelo;
 public class TorrePlateada extends Torre{
 
     public TorrePlateada (Posicion posicion){
-        super(2);
+        super(2, 20);
         this.rango = new Rango(5, posicion);
+    }
+
+    public TorrePlateada(int tiempoDeConstruccion, int costo) {
+        super(tiempoDeConstruccion, costo);
+    }
+
+    public Defensa construir(Jugador jugador, Posicion posicion) {
+        TorrePlateada torreADevolver = new TorrePlateada(posicion);
+        if (jugador.puedeConstruir(super.costo)){
+            jugador.restarCreditos(super.costo);
+            torreADevolver = new TorrePlateada(posicion);
+        }
+        return torreADevolver;
     }
 }
