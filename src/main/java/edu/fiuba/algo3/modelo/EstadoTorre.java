ed1c652e4fe5978174fdb985ec25d355c154e499
mariazanatta <mzanatta@fi.uba.ar>
2023-06-08 17:16:49 -0300

    defender recibe un mapa en vez de un enemigo

--- src/main/java/edu/fiuba/algo3/modelo/EstadoTorre.java
+++ src/main/java/edu/fiuba/algo3/modelo/EstadoTorre.java
@@ -2,6 +2,6 @@ package edu.fiuba.algo3.modelo;
 
 public interface EstadoTorre {
 
-    void defender(Mapa enemigo, int danio, Rango rango);
+    void defender(Mapa mapa, int danio, Rango rango);
     EstadoTorre avanzarTurno();
 }
