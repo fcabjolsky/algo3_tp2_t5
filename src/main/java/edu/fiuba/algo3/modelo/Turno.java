9f5e1ab5e9090f83c31ed9b7307c2d9176eec848
Mateo <mriat@fi.uba.ar>
2023-06-07 21:32:32 -0300

    test: si un jugador elimina todos los enemigos el turno devuelve verdadero

--- src/main/java/edu/fiuba/algo3/modelo/Turno.java
+++ src/main/java/edu/fiuba/algo3/modelo/Turno.java
@@ -13,4 +13,11 @@ public class Turno {
     public void moverEnemigos() {
         this.mapa.pasarTurno();
     }
+
+    public boolean ganoLaPartida() {
+        return !this.mapa.contieneEnemigos();
+    }
+
 }
+
+
--- src/main/test/edu/fiuba/algo3/entrega_1/TurnoTest.java
+++ src/main/test/edu/fiuba/algo3/entrega_1/TurnoTest.java
@@ -1,9 +1,11 @@
 package edu.fiuba.algo3.entrega_1;
 
 import edu.fiuba.algo3.modelo.*;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -24,5 +26,32 @@ public class TurnoTest {
         Posicion nuevaPoscion = enemigo1.obtenerPosicion();
 
         assertEquals(0, nuevaPoscion.calcularDistanciaA(new Posicion(0, 1)));
+    }
+
+    @Test
+    public void unJugadorQueEliminaTodosLosEnemigosDevuelveVerdadero() {
+        Mapa mapa = new Mapa(new ArrayList<Pasarela>(), new ArrayList<Rocoso>(), new ArrayList<Tierra>());
+        Turno turno = new Turno(new Jugador(), mapa);
+
+        boolean resultado = turno.ganoLaPartida();
+
+        Assertions.assertTrue(resultado);
+    }
+
+    @Test
+    public void unJugadorQueMatoATodosLosEnemigosDevuelveVerdadero() {
+        List<Pasarela> pasarelas = new ArrayList<>();
+        Pasarela pasarela = new Pasarela(new Posicion(1,1));
+        Enemigo hormigaMuerta = new Hormiga(new Posicion(1, 1));
+        hormigaMuerta.recibirDanio(1);
+        pasarela.agregarEnemigo(hormigaMuerta);
+        pasarelas.add(pasarela);
+
+        Mapa mapa = new Mapa(pasarelas, new ArrayList<Rocoso>(), new ArrayList<Tierra>());
+        Turno turno = new Turno(new Jugador(), mapa);
+
+        boolean resultado = turno.ganoLaPartida();
+
+        Assertions.assertTrue(resultado);
     }
 }
