2db08ebf353a236ef48fc296eadc008aff3891ca
Caluav <camilaavalos7701@gmail.com>
2023-06-22 18:20:44 -0300

    feat: clase que representa lo visual de una parcela rocosa

---
+++ src/main/java/edu/fiuba/algo3/vista/RocosoView.java
@@ -0,0 +1,32 @@
+package edu.fiuba.algo3.vista;
+
+import edu.fiuba.algo3.modelo.Posicion;
+import edu.fiuba.algo3.vista.Entidad;
+import edu.fiuba.algo3.vista.Tile;
+import javafx.geometry.Pos;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.io.IOException;
+
+public class RocosoView extends Entidad {
+
+    private edu.fiuba.algo3.vista.Tile tile;
+
+    public RocosoView(Posicion posicion) {
+        try{
+            this.tile = new Tile();
+            this.posicion = posicion;
+            this.tile.imagen = ImageIO.read(getClass().getResource("/rocoso.png"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+
+    public void draw(Graphics2D grafico, PanelDePartida panelDePartida){
+        grafico.drawImage(tile.imagen, posicion.getCoordenadaX(), posicion.getCoordenadaY(),
+                panelDePartida.tamanioDelTileAncho, panelDePartida.tamanioDelTileAlto, null);
+    }
+}
