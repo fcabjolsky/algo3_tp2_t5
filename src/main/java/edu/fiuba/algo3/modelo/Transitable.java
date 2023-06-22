f3f58757d949e95ce9849244716ceb7b33ba8b3d
Mateo <mriat@fi.uba.ar>
2023-06-22 16:34:22 -0300

    feat: ahora las clases que implementan Transitable entienden el mensaje contieneEnemigos

--- src/main/java/edu/fiuba/algo3/modelo/Transitable.java
+++ src/main/java/edu/fiuba/algo3/modelo/Transitable.java
@@ -4,4 +4,6 @@ public interface Transitable {
     public void recibirEnemigo(Enemigo enemigo);//deberia llamarse recibir movible pero la lista que guarda las clases que implementan transitable son de Enemigos no de Movibles
 
     public void moverEnemigosA(Transitable otraParcela);
+
+    public boolean contieneEnemigos();
 }
