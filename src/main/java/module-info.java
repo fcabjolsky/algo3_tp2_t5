6ebf1576e7cda50e9d36659cefc3a0c36aba2be5
Caluav <camilaavalos7701@gmail.com>
2023-06-22 18:22:12 -0300

    feat: se modifico para incluir javafx-fxml

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,5 +1,13 @@
 module edu.fiuba.algo3 {
     requires javafx.controls;
     requires org.json;
+    requires java.desktop;
+    requires javafx.base;
+    requires javafx.graphics;
+    requires java.logging;
+    requires javafx.fxml;
+    requires java.sql;
+
+    opens edu.fiuba.algo3.vista to javafx.fxml;
     exports edu.fiuba.algo3;
 }
