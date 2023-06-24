91fad30c7a2d6d7d3f915b8028cbc3cac9f4d9c8
Caluav <camilaavalos7701@gmail.com>
2023-06-23 23:34:07 -0300

    feat: se añadio para poder seleccionar una parcela y que se iluminen los bordes cuando el mouse entra en una

--- src/main/java/edu/fiuba/algo3/vista/ControladorMouse.java
+++ src/main/java/edu/fiuba/algo3/vista/ControladorMouse.java
@@ -1,5 +1,10 @@
 package edu.fiuba.algo3.vista;
 
+import javax.swing.*;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.Border;
+import javax.swing.border.LineBorder;
+import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
@@ -14,5 +19,15 @@ public class ControladorMouse extends MouseAdapter {
     @Override
     public void mousePressed(MouseEvent e) {
         System.out.println("x= "+ parcela.getX()+"y="+parcela.getY());
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        this.parcela.setBorder(new LineBorder(Color.CYAN));
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        this.parcela.setBorder(BorderFactory.createEmptyBorder());
     }
 }
