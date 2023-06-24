4685eb9416330845ff9f919e2e7938256cd74b5e
Caluav <camilaavalos7701@gmail.com>
2023-06-23 23:35:04 -0300

    feat: ahora se encarga de pintarse y es un JPanel

--- src/main/java/edu/fiuba/algo3/vista/Entidad.java
+++ src/main/java/edu/fiuba/algo3/vista/Entidad.java
@@ -2,17 +2,51 @@ package edu.fiuba.algo3.vista;
 
 import edu.fiuba.algo3.modelo.Posicion;
 
+import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
 
-public abstract class Entidad {
+public abstract class Entidad extends JPanel implements Runnable {
 
     protected Posicion posicion;
-
-    public abstract void draw(Graphics2D grafico, PanelDePartida panelDePartida);
+    private Thread threadJuego;
+    protected BufferedImage imagen;
 
     public void setPosicion(Posicion posicion) {
         this.posicion = posicion;
     }
 
-    public abstract Entidad devolverNuevaInstancia(Posicion posicion);
+    public Entidad(Posicion posicion, int anchoTile, int altoTile){
+        this.posicion = posicion;
+        this.setPreferredSize(new Dimension(anchoTile, altoTile));
+        this.setDoubleBuffered(true);
+        this.setBackground(Color.BLACK);
+        addMouseListener(new ControladorMouse(this));
+        this.empezarThreadDeJuego();
+    }
+    public abstract Entidad devolverNuevaInstancia(Posicion posicion, int anchoTile, int altoTile);
+
+    @Override
+    public void paintComponent(Graphics grafico){
+        super.paintComponent(grafico);
+        Graphics2D grafico2D = (Graphics2D)grafico;
+        grafico.drawImage(this.imagen, 0, 0,
+                this.getWidth(), this.getHeight(), this);
+    }
+    private void empezarThreadDeJuego(){
+        threadJuego = new Thread(this);
+        threadJuego.start();
+    }
+    @Override
+    public void run() {
+        while (threadJuego != null){
+            //1- actualizar informacion de mapa.
+            this.actualizar();
+            //2- dibujar la escena.
+            this.repaint();
+        }
+    }
+
+    public void actualizar(){
+    }
 }
