aed54670facd8c8f96680dbdea79bcb6c5b54711
Mateo <mriat@fi.uba.ar>
2023-06-26 23:47:04 -0300

    feat: ahora Partida implementa Turneable y ademas agregue un constructor que recibe un jugador y un observador

--- src/main/java/edu/fiuba/algo3/modelo/Partida.java
+++ src/main/java/edu/fiuba/algo3/modelo/Partida.java
@@ -1,17 +1,16 @@
 package edu.fiuba.algo3.modelo;
 
-public class Partida extends Observable {
+public class Partida extends Observable implements Turneable{
 
-
-    private final Mapa mapa;
+    private final Mapa mapa; //esto podria salir de aca y estar dentro de Turno directamente
 
     private final Jugador jugador;
     private Observador logger;
-
     private Turno turno;
-  
-    
-    public Partida(Mapa mapa, Jugador jugador) {
+    private CreadorMapaJson creadorMapa;
+
+
+    public Partida(Mapa mapa, Jugador jugador) {//hace alusion al empezarPartida, que el jugador tenga una partida en vez de un turno, a la que le diga avanzarTurno
     	this.mapa = mapa;
         this.jugador = jugador;
         this.turno = new Turno(jugador, mapa);
@@ -25,11 +24,27 @@ public class Partida extends Observable {
         this.agregarObservador(logger);
         turno.agregarObservador(this.logger);
     }
-    /*public void jugar() {
-        while (!this.turno.ganoLaPartida()) {
-            this.turno;
+
+    public Partida(Jugador jugador, Observador logger) {
+        this.jugador = jugador;
+        this.creadorMapa = new CreadorMapaJson("src/main/java/edu/fiuba/algo3/modelo/mapa.json");
+        this.mapa = this.creadorMapa.crearMapa();
+        this.turno = new Turno(this.jugador, this.mapa);
+        this.agregarObservador(logger);
+        turno.agregarObservador(this.logger);
+    }
+
+    public String jugar() {
+        boolean juegoTerminado = false;
+        while (!juegoTerminado) {
+            try {
+                this.turno.siguienteTurno();
+            } catch (JuegoGanado juegoGanado) {
+                return this.juegoGanado();
+            }
         }
-    }*/
+        return "ERROR";
+    }
 
     public String juegoGanado() {
         this.setearCambiado();
@@ -40,8 +55,15 @@ public class Partida extends Observable {
         this.notificarObservadores("Seguir Jugando");
         return "SEGUIR JUGANDO";
     }
-   
 
-    
+    @Override
+    public void avanzarTurno() {
+        try {
+            this.turno.siguienteTurno();
+        } catch (JuegoGanado juegoGanado) {
+            this.setearCambiado();
+            this.notificarObservadores("Ganaste");
+        }
 
+    }
 }
