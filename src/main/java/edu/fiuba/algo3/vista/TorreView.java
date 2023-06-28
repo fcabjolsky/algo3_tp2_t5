aa5cb35646c6748b3813ab1b8a4af40ab41abf6b
Caluav <camilaavalos7701@gmail.com>
2023-06-28 18:50:07 -0300

    feat: hereda de defensaView, tiene una animacion de disparo, la llama cuando se actualiza para atacar

---
+++ src/main/java/edu/fiuba/algo3/vista/TorreView.java
@@ -0,0 +1,17 @@
+package edu.fiuba.algo3.vista;
+
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+
+public class TorreView extends DefensaView{
+
+    private DisparoAnimacion ataque;
+    public TorreView(String urlTorreImagen, int anchoTile, int altoTile, int x, int y) {
+        super(urlTorreImagen, anchoTile, altoTile, x, y);
+        ataque = new DisparoAnimacion(this, anchoTile, altoTile);
+    }
+
+    public void update(Pane contenedor, GridPane mapa, int posicionParcelaAtacadaX, int posicionParcelaAtacadaY){
+        ataque.realizarAtaque(contenedor, mapa, posicionParcelaAtacadaX, posicionParcelaAtacadaY);
+    }
+}
