ce3254127273c969b46ccc334fb265badd029e6d
Mateo <mriat@fi.uba.ar>
2023-07-03 05:10:17 -0300

    refactor: ahora el enemigo implementa el patron State, puede estar Vivo Muerto o Eliminado

--- src/main/java/edu/fiuba/algo3/modelo/Enemigo.java
+++ src/main/java/edu/fiuba/algo3/modelo/Enemigo.java
@@ -2,34 +2,50 @@ package edu.fiuba.algo3.modelo;
 
 public abstract class Enemigo extends Observable {
     protected int energia;
+
     protected int velocidad;
-    protected Movible estado;
+
+    protected Movible estado; //esto en un futuro puede ser un State dentro de Movimiento, ya que es un estado del mismo
+
     protected int danio;
 
+    private EstadoEnemigo estadoEnemigo;
+
+    public Enemigo() {
+        this.nuevoEstado(new EstadoVivo());
+    }
+
+    public void nuevoEstado(EstadoEnemigo nuevoEstado) {
+        this.estadoEnemigo = nuevoEstado;
+        this.estadoEnemigo.setEnemigo(this);
+    }
+
     public void recibirDanio(int unDanio) {
-        if (this.estaMuerta()) {
-            throw new EnemigoMuerto();
-        }
-        this.energia -= unDanio;
-        this.notificarObservadores("Enemigo " + this.toString() + " recibe danio " + unDanio + " Vida restante " + this.energia);
+        this.estadoEnemigo.recibirDanio(unDanio);
     }
 
     public boolean estaMuerta() {
-        return this.energia <= 0;
+        return this.estadoEnemigo.estaMuerto();
     }
 
     public abstract void morir(Jugador jugador);
 
+    public int morir() {
+        return this.estadoEnemigo.morir();
+    } //el nombre del mensaje morir() tal vez podria cambiar a uno mas representativo
+
+    public abstract int darRecompensa();
+
     public void avanzar(Transitable siguienteTransitable) {
-        this.estado = this.estado.moverA(this, siguienteTransitable);
+        this.estadoEnemigo.avanzar(siguienteTransitable);
     }
 
     public void avanzarTurno() {
         this.estado = new EnMovimiento(this.velocidad);
     }
 
-    public int atacar(int numeroTurno){
-        return this.danio;
+    public int atacar(){
+        return this.estadoEnemigo.atacar();
     }
 
     public boolean sePuedeMover() {
