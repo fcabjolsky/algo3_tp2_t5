06a2e6eeaee77450f9d4e708cc8eb259fb3bd096
Mateo <mriat@fi.uba.ar>
2023-07-04 05:07:43 -0300

    refactor: ahora la hormiga tiene un atributo tipo interface (AtaqueEnemigo) en el que delega su ataque

--- src/main/java/edu/fiuba/algo3/modelo/Hormiga.java
+++ src/main/java/edu/fiuba/algo3/modelo/Hormiga.java
@@ -8,6 +8,7 @@ public class Hormiga extends Enemigo {
         this.energia = 1;
         this.velocidad = 1;
         this.estado = new EnMovimiento(this.velocidad);
+        this.ataqueEnemigo = new AtaqueEnemigoNormal();
         this.danio = 1;
         this.contadorDeMuertesDeHormiga = ContadorDeMuertesDeHormiga.obtenerContador();
     }
