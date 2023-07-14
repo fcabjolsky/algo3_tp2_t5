bcb07a00b2b9b7364f21dc0952a73322ffd8bbed
Mateo <mriat@fi.uba.ar>
2023-07-13 21:59:51 -0300

    las interfaces Defensa y EstadoTorre extienden de la interface Turneable

--- src/main/java/edu/fiuba/algo3/modelo/Defensa.java
+++ src/main/java/edu/fiuba/algo3/modelo/Defensa.java
@@ -1,14 +1,12 @@
 package edu.fiuba.algo3.modelo;
 
-public interface Defensa{
+public interface Defensa extends Turneable{
 
     public Defensa construir(Jugador jugador);
 
     public void defender(Pasarela pasarela);
 
     public boolean puedeConstruir(int creditos);
-
-    public void avanzarTurno();
 
     public int getCosto();
 
